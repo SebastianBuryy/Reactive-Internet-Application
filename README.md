2b560126c4e4478f7cef8edcf73e42ddf3eb7410
Sebastian Bury <sebastianbury2004@gmail.com>
2025-10-17 21:52:12 +0100

    added geocode api

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Reactive-Internet-Application
 
+### Notes
+
+Instead of prompting a user for a city like 'London', and then executing the query, the system waits for input, and will provide autocomplete options based on the inputted characters.
+This will reduce ambiguity, for example, between 'London, Ontario, CA' and 'London, England, GB'.
+
 ### Quickstart Guide
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -10,4 +12,17 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
 
+const apiKey = process.env.OPENWEATHER_API_KEY
 
+// Geocoding API endpoint
+app.get('/api/geocode', async (req, res) => {
+    try {
+        const city = 'London'
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
+        const data = await response.json()
+        res.json(data)
+    } catch (error) {
+        console.error('Error fetching geocode data:', error)
+        res.status(500).json({ error: 'Failed to fetch geocode data' })
+    }
+})
